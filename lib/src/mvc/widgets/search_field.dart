5731a25eaeae5c77930765c5204cf80509de7b35
RoeiGida <roeiroei77777@gmail.com>
2022-04-04 16:04:52 +0300

    alignment

--- lib/src/mvc/widgets/search_field.dart
+++ lib/src/mvc/widgets/search_field.dart
@@ -34,54 +34,58 @@ class SearchFiled extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: AlignmentDirectional.topCenter,
       children:[
-        Center(child: Container(child: Image.asset('assets/ui/images/thinking_woman_purple_bg.png'))),
-        Container(
-        alignment: Alignment.center,
-        height: 42.0,
-        width: 304,
-        margin: showSearchIcon!
-            ? const EdgeInsets.only(bottom: 3.5, top: 3.5, right: 2.0, left: 2.0)
-            : isSearching!
-                ? const EdgeInsets.only(bottom: 3.5, top: 3.5, right: 10.0)
-                : const EdgeInsets.only(
-                    bottom: 3.5, top: 3.5, right: 10.0, left: 10.0),
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14.0),
-          color: searchBackgroundColor ?? Colors.blueGrey.withOpacity(.2),
-        ),
-        child: TextField(
-          controller: searchQueryController,
-          focusNode: searchFocusNode,
-          decoration: InputDecoration(
-            hintText: hintText == null ? "Search...":hintText,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14.0),
-            ),
-            enabledBorder: InputBorder.none,
-            focusedBorder: InputBorder.none,
-            hintStyle: TextStyle(color: searchTextHintColor),
-            suffixIcon: searchQueryController!.text.isNotEmpty
-                ? IconButton(
-                    alignment: Alignment.centerRight,
-                    color: clearSearchButtonColor,
-                    icon: const Icon(Icons.clear),
-                    onPressed: onClearButtonPressed!,
-                  )
-                : const SizedBox(
-                    height: 0.0,
-                    width: 0.0,
-                  ),
+        Center(child: Container(child: Image.asset('assets/ui/images/thinking_woman_purple_bg.png',width: 328,height: 216,))),
+        Positioned(
+          top: 12,
+          child: Container(
+          alignment: Alignment.center,
+          height: 42.0,
+          width: 304,
+          margin: showSearchIcon!
+              ? const EdgeInsets.only(bottom: 3.5, top: 3.5, right: 2.0, left: 2.0)
+              : isSearching!
+                  ? const EdgeInsets.only(bottom: 3.5, top: 3.5, right: 10.0)
+                  : const EdgeInsets.only(
+                      bottom: 3.5, top: 3.5, right: 10.0, left: 10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14.0),
+            color: searchBackgroundColor ?? Colors.blueGrey.withOpacity(.2),
           ),
-          textAlignVertical: TextAlignVertical.center,
-          textInputAction: TextInputAction.search,
-          style: TextStyle(color: searchTextColor, fontSize: 16.0),
-          onChanged: (query) => onSearchQueryChanged!(query),
-          // onSubmitted: (query) => onSearchQueryUpdated!(query),
-          // onEditingComplete: () => onEditingComplete!,
-        ),
-      ),]
+          child: TextField(
+            controller: searchQueryController,
+            focusNode: searchFocusNode,
+            decoration: InputDecoration(
+              hintText: hintText == null ? "Search...":hintText,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(14.0),
+              ),
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              hintStyle: TextStyle(color: searchTextHintColor),
+              suffixIcon: searchQueryController!.text.isNotEmpty
+                  ? IconButton(
+                      alignment: Alignment.centerRight,
+                      color: clearSearchButtonColor,
+                      icon: const Icon(Icons.clear),
+                      onPressed: onClearButtonPressed!,
+                    )
+                  : const SizedBox(
+                      height: 0.0,
+                      width: 0.0,
+                    ),
+            ),
+            textAlignVertical: TextAlignVertical.center,
+            textInputAction: TextInputAction.search,
+            style: TextStyle(color: searchTextColor, fontSize: 16.0),
+            onChanged: (query) => onSearchQueryChanged!(query),
+            // onSubmitted: (query) => onSearchQueryUpdated!(query),
+            // onEditingComplete: () => onEditingComplete!,
+          ),
+      ),
+        ),]
     );
   }
 }
