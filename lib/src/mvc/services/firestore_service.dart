b341ee3284d52a869fb0173cadb0c0e14dd11dc1
RoeiGida <roeiroei77777@gmail.com>
2022-04-05 13:02:52 +0300

    Changed hebrew service

--- lib/src/mvc/services/firestore_service.dart
+++ lib/src/mvc/services/firestore_service.dart
@@ -36,7 +36,7 @@ class FirestoreService<T> {
       return collectionReference
           .orderBy('$searchBy', descending: false)
           .where('$searchBy', isGreaterThanOrEqualTo: query)
-      // .where('$searchBy', isLessThan: query + 'z')
+      .where('$searchBy', isLessThan: query + 'ת')
           .limit(limitOfRetrievedData!)
           .snapshots()
           .map(dataListFromSnapshot!);
