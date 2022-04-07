b10fce325326e2ff27c1bfc60359b78629b18e06
RoeiGida <roeiroei77777@gmail.com>
2022-04-07 20:30:30 +0300

    container padding

--- lib/src/mvc/widgets/search_field.dart
+++ lib/src/mvc/widgets/search_field.dart
@@ -63,7 +63,7 @@ class SearchFiled extends StatelessWidget {
                   ? const EdgeInsets.only(bottom: 3.5, top: 3.5, right: 10.0)
                   : const EdgeInsets.only(
                       bottom: 3.5, top: 3.5, right: 10.0, left: 10.0),*/
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          // padding: const EdgeInsets.symmetric(horizontal: 8.0),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(14.0),
             color: searchBackgroundColor ?? Colors.blueGrey.withOpacity(.2),
