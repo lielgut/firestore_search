1f96a400df12225d64cc7bd4b69902a6f8b45598
RoeiGida <roeiroei77777@gmail.com>
2022-04-25 13:43:37 +0300

    Test for no changes of same query

--- lib/src/mvc/controllers/firestore_search_controller.dart
+++ lib/src/mvc/controllers/firestore_search_controller.dart
@@ -7,7 +7,7 @@ class FirestoreSearchController extends GetxController {
   Rx<TextEditingController> searchQueryController = TextEditingController().obs;
 
   void updateSearchQuery(String newQuery) {
-    if (searchQuery.value == newQuery) return;
+    if (searchQuery.value.toString() == newQuery) return;
     searchQuery.value = newQuery;
     update();
   }
